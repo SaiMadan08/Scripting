70df141cb51a7271aec19816a1d731ea0ad5de76
SaiMadan8 <saimadanv@gmail.com>
2023-12-15 18:51:35 +0530

    Shell scripting

---
+++ 05-datatypes.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+NUMBER1=$1
+NUMBER2=$2
+
+SUM=$(($NUMBER1+$NUMBER2))
+SUB=$(($NUMBER1-$NUMBER2))
+MUL=$(($NUMBER1*$NUMBER2))
+
+echo "Sum is : $SUM
+      Sub is : $SUB
+      MUL is : $MUL"
+echo "How many args passed : $#"
+
+echo "All args passed : $@"
+
+echo "Script name : $0"
