de96f006f3af9cd246c07110b5c2df4d6577f1a0
SaiMadan8 <saimadanv@gmail.com>
2023-12-17 10:57:37 +0530

    Shell scripting

---
+++ 09-functions.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+VALIDATE(){
+    if[$1 -ne 0]
+    then
+        echo " ERROR $2 is Failed"
+        exit 1
+    else
+        echo "$2 is Succuess"
+    fi
+}
+
+if [$ID -ne 0]
+then
+    echo "You are not a root user"
+    exit 1
+else
+    echo " You are a root user"
+fi
+
+yum install mysql -y
+
+VALIDATE $? "Installing Mysql"
+
+Yum install git -y
+
+VALIDATE $? "Installing git"
+
