4757daa240435674c7e846fd8ef38b2ca001c80c
SaiMadan8 <saimadanv@gmail.com>
2023-12-17 11:01:47 +0530

    Shell scripting

--- 09-functions.sh
+++ 09-functions.sh
@@ -4,7 +4,7 @@ ID=$(id -u)
 
 VALIDATE(){
     if[$1 -ne 0]
-    then
+    
         echo " ERROR $2 is Failed"
         exit 1
     else
@@ -13,7 +13,7 @@ VALIDATE(){
 }
 
 if [$ID -ne 0]
-then
+
     echo "You are not a root user"
     exit 1
 else
