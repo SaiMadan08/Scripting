a0454932ba6257b2642263b2f68e9d9dfd6e40ec
SaiMadan8 <saimadanv@gmail.com>
2023-12-17 11:53:03 +0530

    Shell scripting

---
+++ 12-install-packages.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE=/tmp/$0-$TIMESTAMP.log
+
+echo "Script started at $TIMESTAMP"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$R ERROR $2 is FAILED $N"
+    else
+        echo -e "$G $2 is SUCCESS $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R You are not a root user $N"
+else
+    echo -e "$G You are a root user $N"
+fi
+
+for package in $@
+do
+    yum list installed $package &>> $LOGFILE
+    if [ $? -ne 0 ]
+    then
+        yum install $package -y &>> $LOGFILE
+        VALIDATE $? " INSTALLING $package"
+    else
+        echo -e " $package is already installed $Y SKIPPING $N"
+    fi
+done
+
